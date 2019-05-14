6c646254bc1c6589b27e0db2961d6435fd08a144
Tobias Carlander <tobias.carlander@wfp.org>
2019-05-14 17:14:11 +0300

    reduse

--- postgres/fields/__init__.py
+++ postgres/fields/__init__.py
@@ -1,6 +1,1 @@
-from .composite_field import *              # NOQA
 from .json_field import JSONField           # NOQA
-from .interval_field import IntervalField   # NOQA
-from .range_fields import Int4RangeField    # NOQA
-from .ts_fields import TSVectorField        # NOQA
-from .uuid_field import UUIDField           # NOQA
